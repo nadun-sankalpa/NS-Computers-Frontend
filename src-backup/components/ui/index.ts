3b3b009a9df79b438b9548ba3b63d8fa2bc31109
Nadun <nadunsankalpadesilva27@gmail.com>
2025-08-03 01:22:51 +0530

    Admin side Operations Done

--- src-backup/components/ui/index.ts
+++
@@ -1,8 +0,0 @@
-export { AuthNotification } from './AuthNotification';
-export * from './button';
-export * from './input';
-export * from './textarea';
-export * from './card';
-export * from './dialog';
-export * from './select';
-export * from './label';
